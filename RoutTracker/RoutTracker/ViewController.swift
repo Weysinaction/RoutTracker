a0c11a4fa2098a6f6df097bc3a6b09eef91b734f
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-09-29 16:17:44 +0300

    init

---
+++ RoutTracker/RoutTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoutTracker
+//
+//  Created by Владислав Лазарев on 29.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
